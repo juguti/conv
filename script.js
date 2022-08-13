af089baeef8641e7bdee11e4c8aa1fd2319ff3bc
juguti <gbussac77@gmail.com>
2022-08-13 16:13:50 +0200

    final maybe
    13/08/22

--- script.js
+++ script.js
@@ -32,11 +32,48 @@ function bidi(params) {
         if (n=='1') {
             de+=2**puiss;
             puiss-=1;}
-        else{puiss-=1}
-
-    }
+        else{puiss-=1} }
     console.log(de)
     resubi.textContent=de
 
     
 };
+function dihe(params) {
+    let co=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+    let chhe=document.getElementById("chhe").value
+    let q=~~(chhe/16)
+    let r=chhe-q*16
+    let resu=(co[q]+co[r])
+    console.log(q)
+    console.log(r)
+    console.log(resu)
+    document.getElementById("resuhe").textContent=resu
+    
+
+    
+};
+function hedi(params) {
+    let ch=0;
+    let coinv={"A":10,"B":11,"C":12,"D":13,"E":14,"F":15};
+    let number=["0","1","2","3","4","5","6","7","8","9"];
+    let chhe=document.getElementById("chhe").value;
+    let puiss =chhe.length-1;
+    console.log(chhe)
+    for (const n of chhe) {
+        
+        if (!(n in number)) {
+            let nn=coinv[n];
+            console.log(nn);
+            console.log("out");
+            ch+=nn*16**puiss;
+            puiss-=1; 
+            continue;}
+            ch+=n*16**puiss;
+            puiss-=1; 
+            
+            
+        }
+    console.log(ch)
+    document.getElementById("resuhe").textContent=ch
+
+}
